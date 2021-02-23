a86a41793eef4124312cdaafa1689f6dfd2c5423
Julien Deniau <julien.deniau@gmail.com>
2021-02-23 19:51:19 +0100

    fix hostname

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://julien.deniau.me/xbox-' : '',
+  assetPrefix: isProd ? 'https://jdeniau.github.io/xbox-game-pass-rating/' : '',
 };
