32c4b44b0dafc92ba97fc08263794cfb033b0d3e
Julien Deniau <julien.deniau@gmail.com>
2021-10-13 10:37:28 +0200

    upgrade deps + redirect from / to /games

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   // Use the CDN in production and localhost for development.
   basePath: isProd ? '/xbox-game-pass-ratings' : '',
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/games',
+        permanent: true,
+      },
+    ];
+  },
 };
